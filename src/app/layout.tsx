1bf15da1ffbe43a6e40c40ffb15f9154d632b184
Andrew Vergara <drewvergara@gmail.com>
2025-01-17 15:22:55 -0800

    Adding in metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,34 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Prelude",
   description: "Prelude is an applet that is a minimalistic countdown timer.",
+  openGraph: {
+    title: 'Prelude',
+    description: 'Prelude is an applet that is a minimalistic countdown timer.',
+    url: 'https://prelude-offekt.vercel.app/',
+    siteName: 'Prelude',
+    images: [
+      {
+        url: 'https://offekt.s3.us-west-2.amazonaws.com/opengraph/prelude_opengraph_800x600.png', // Must be an absolute URL
+        width: 800,
+        height: 600,
+      },
+      {
+        url: 'https://offekt.s3.us-west-2.amazonaws.com/opengraph/prelude_opengraph_1800x1600.png', // Must be an absolute URL
+        width: 1800,
+        height: 1600,
+        alt: 'Prelude | Minimalistic Countdown Timer',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Prelude',
+    description: 'Prelude is an applet that is a minimalistic countdown timer.',
+    creator: '@drewvergara',
+    images: ['https://offekt.s3.us-west-2.amazonaws.com/opengraph/prelude_opengraph_1800x1600.png'], // Must be an absolute URL
+  }    
 };
 
 export default function RootLayout({
